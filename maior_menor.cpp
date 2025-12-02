2d63ab45bfdadb418763e5e6e7cb69463c2cee50
Guilherme Nunes <guiilucasdev@gmail.com>
2025-12-02 17:08:34 -0300

    Add files via upload

---
+++ maior_menor.cpp
@@ -0,0 +1,34 @@
+
+#include <iostream>
+
+int main()
+{
+	const int tamanho = 8;
+	int numeros[tamanho];
+	int maior = -1000000, menor = 1000000;
+	int posicaoMaior = 0, posicaoMenor = 0;
+	
+	for(int i = 0; i < tamanho; i++)
+	{
+		std::cout << "Digite o " << i + 1 << "º número: ";
+		std::cin >> numeros[i];
+	}
+	
+	for(int i = 0; i < tamanho; i++)
+	{
+		if(numeros[i] > maior)
+		{
+			maior = numeros[i];
+			posicaoMaior = i;
+		}
+		
+		if(numeros[i] < menor)
+		{
+			menor = numeros[i];
+			posicaoMenor = i;
+		}
+	}
+	
+	std::cout << "Maior número: " << maior << "| Posição: " << posicaoMaior << std::endl;
+	std::cout << "Menor número: " << menor << "| Posição: " << posicaoMenor << std::endl;
+}
